e911b092372328cd4a113491f7dc3a846e24b8c4
imjovi@qq.com <onlylove77>
2021-07-01 21:32:47 +0800

    更新版本

--- src/main/java/cn/vonce/common/annotation/LogContent.java
+++ src/main/java/cn/vonce/common/annotation/LogContent.java
@@ -2,12 +2,15 @@ package cn.vonce.common.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 日志内容
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @Documented
 @Inherited
 public @interface LogContent {
 
-	String value();
+    String value();
 
 }
